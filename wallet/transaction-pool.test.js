d5f3e9ad334381c0b608d81efaca602f141fd5f1
unknown <alexander.dragomir@gmail.com>
2019-09-17 20:04:39 +0300

    Transaction Updates in the API

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,12 +3,13 @@ const Transaction = require('./transaction');
 const Wallet = require('./index');
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool();
+        senderWallet = new Wallet();
         transaction = new Transaction({
-            senderWallet: new Wallet(), recipient: 'fake-recipient', amount: 50
+            senderWallet, recipient: 'fake-recipient', amount: 50
         });
     });
 
@@ -17,6 +18,18 @@ describe('TransactionPool', () => {
             transactionPool.setTransaction(transaction);
 
             expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+        });
+    });
+
+    describe('existingTransaction', () => {
+        it('return an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction);
+
+            expect(transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey })).toBe(transaction);
+        });
+
+        it('', () => {
+
         });
     });
 });
